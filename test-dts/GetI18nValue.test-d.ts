191435494d5037605378fb7a08cc106b821718bb
liaoyl <liaoyl@akulaku.com>
2021-07-07 18:58:46 +0800

    :white_check_mark: Adding tests.

--- test-dts/GetI18nValue.test-d.ts
+++ test-dts/GetI18nValue.test-d.ts
@@ -2,23 +2,22 @@ import { GetI18nValue } from "../src/utils/GetI18nValue";
 import { expectType } from "tsd";
 
 const resources = {
-  en: {
-    global: {
-      noParams: "Hello ",
-      paramsOne: "Hello {{one}}",
-      paramsTwo: "Hello {{one}} {{tow}} ",
-      paramsThree: "Hello {{one}} {{tow}} {{three}}",
-      paramsThree2: "Hello {{one}} {{tow}} {{three}} end",
-    },
-    login: {
-      noParams: "Hello ",
-      success: "Hello {{one}}",
-      failure: "Hello {{one}} {{tow}} ",
-    },
+  global: {
+    noParams: "Hello",
+    paramsOne: "Hello {{one}}",
+    paramsTwo: "Hello {{one}} {{tow}} ",
+  },
+  login: {
+    noParams: "Hello ",
+    success: "Hello {{one}}",
+    failure: "Hello {{one}} {{tow}} ",
   },
 } as const;
+type Data = typeof resources;
+type T1 = GetI18nValue<"global:noParams", Data>;  // "Hello"
+type T2 = GetI18nValue<"global:paramsTwo", Data>; // "Hello {{one}} {{tow}} "
+type T3 = GetI18nValue<"login:success", Data>;    // "Hello {{one}}"
 
-type Data = typeof resources.en;
 declare const getI18nValue: <T>(str: T) => GetI18nValue<T, Data>;
 
 expectType<Data["global"]["noParams"]>(getI18nValue("global:noParams"));
